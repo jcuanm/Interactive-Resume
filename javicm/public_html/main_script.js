4908183621bb9b78b5b4be3dd5bc8c9eb0d43f09
Javier Cuan-Martinez <jcuanm@gmail.com>
2016-01-13 12:15:54 -0800

    Adding the files

---
+++ javicm/public_html/main_script.js
@@ -0,0 +1,24 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(function(){
+    
+    $('a[href^="#"]').click(function(e){
+        
+        e.preventDefault();
+        
+        var target = $(this).attr('href');  
+        var strip = target.slice(1);
+        var anchor = $("a[name='" + strip + "']");
+        
+        $('html,body').animate({
+            
+           scrollTop: anchor.offset().top
+            
+        },'slow');
+        
+    });
+});
